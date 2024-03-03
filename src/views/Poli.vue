bed5029f0866114c30077cc95be1ff182dd9ab01
sheeranada <ibj.prod001@gmail.com>
2024-03-03 19:29:10 +0700

    log data sudah benar

--- src/views/Poli.vue
+++ src/views/Poli.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import api from '../api/endpoint';
-
+import axios from 'axios'
 
 const ralan = ref([]);
 
@@ -21,7 +21,7 @@ const fetchDataRalan = async () => {
                     {
                         data: null,
                         render: function (data, type, row) {
-                            return '<button type="button" class="btn btn-primary btn-detail">Detail</button>';
+                            return '<button type="button" class="btn btn-secondary btn-detail"><i class="fas fa-play"></i></button>';
                         }
                     }
                 ]
@@ -30,6 +30,16 @@ const fetchDataRalan = async () => {
 }
 onMounted(() => {
     fetchDataRalan()
+
+    // listener e poli ambek pasien
+    let selectedPoli = ''; // Menyimpan nomor poli yang dipilih
+
+    // Event listener for select element
+    const selectPoli = document.getElementById('poli');
+    selectPoli.addEventListener('change', function (event) {
+        selectedPoli = event.target.value;
+    });
+
     // Event listener untuk tombol Detail
     $(document).on('click', '.btn-detail', function () {
         // Mengambil data nm_pasien (kolom kedua)
@@ -38,29 +48,65 @@ onMounted(() => {
         // Mengambil data nm_dokter (kolom keempat)
         var nmDokter = $(this).closest('tr').find('td:eq(3)').text();
 
-        // Menampilkan data dengan alert
-        alert("Nama Pasien: " + nmPasien + "\nNama Dokter: " + nmDokter);
+        var data = {
+            "poli": selectedPoli,
+            "nm_pasien": nmPasien,
+            "dokter": nmDokter,
+        }
+        var jsonData = JSON.stringify(data)
+        console.log(jsonData)
     });
 })
-
 </script>
+
+
 
 <template>
     <div>
         <h1>ini halaman poli</h1>
-        <table id="myTable" class="table table-striped table-hover table-bordered table-responsive-sm">
-            <thead>
-                <tr>
-                    <th>TGL REGISTRASI</th>
-                    <th>NAMA PASIEN</th>
-                    <th>NO RM</th>
-                    <th>DOKTER</th>
-                    <th>PILIHAN</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div class="container">
+            <div class="row flex-row-reverse">
+                <div class="col-3">
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="poli">Pilih Poli</label>
+                        <select class="form-select" id="poli" name="poli">
+                            <option selected disabled>Nomor...</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container px-5">
+            <div class="row">
+                <div class="col">
+                    <table id="myTable" class="table table-striped table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th>TGL REGISTRASI</th>
+                                <th>NAMA PASIEN</th>
+                                <th>NO RM</th>
+                                <th>DOKTER</th>
+                                <th>PILIHAN</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-            </tbody>
-        </table>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
